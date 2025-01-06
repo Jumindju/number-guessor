3225019890bb7aa47482a11c1d9278056b35905a
Justus Hueske <justus.hueske@studmail.w-hs.de>
2025-01-06 22:56:34 +0100

    Finish displaying timeline

--- src/app/components/time-line/time-line.component.ts
+++ src/app/components/time-line/time-line.component.ts
@@ -1,5 +1,5 @@
 import { DecimalPipe } from '@angular/common';
-import { Component, effect, input, signal } from '@angular/core';
+import { Component, computed, effect, input, signal } from '@angular/core';
 
 @Component({
   selector: 'app-time-line',
@@ -11,8 +11,14 @@ export class TimeLineComponent {
   min = input.required<number>();
   max = input.required<number>();
   markerCount = input<number>();
+  indicatorPerMarker = input<number>();
+  indicatorMid = computed(() => {
+    const indicatorPerMarker = this.indicatorPerMarker() ?? 0;
+    return (indicatorPerMarker / 2) - 1;
+  });
 
   markers = signal<number[]>([]);
+  indicators = signal<number[]>([]);
 
   constructor() {
     effect(() => {
@@ -20,14 +26,30 @@ export class TimeLineComponent {
       const max = this.max();
 
       const markerAmount = this.markerCount() ?? max;
-      const markerSteps = max / markerAmount;
+      const markerStep = max / markerAmount;
+
+      const indicatorPerMarker = this.indicatorPerMarker() ?? 1;
+      const indicatorStep = markerStep / indicatorPerMarker;
 
       const newMarkers = [];
-      for (let i = min; i <= max; i += markerSteps) {
-        newMarkers.push(i);
+      const newIndicators: number[] = [];
+
+      for (let marker = min; marker <= max; marker += markerStep) {
+        newMarkers.push(marker);
+
+        if (indicatorPerMarker === 1 || marker === max) {
+          continue;
+        }
+
+        const markerPercent = (marker / max) * 100;
+        const indicatorPercent = (indicatorStep / max) * 100;
+        for (let indicator = 1; indicator < indicatorPerMarker; indicator++) {
+          newIndicators.push(markerPercent + indicatorPercent * indicator);
+        }
       }
 
       this.markers.set(newMarkers);
+      this.indicators.set(newIndicators);
     });
   }
 }
